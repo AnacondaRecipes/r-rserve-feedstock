818afb036ae2246e1ee538dfdff2a5ccc1d0ed39
Ray Donnelly <mingw.android@gmail.com>
2018-01-03 14:29:57 +0000

    Add my comments back, I added them for a reason

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-if [[ $(uname) == Darwin ]]; then
+# This package makes a standalone binary and needs to find libraries essential to R.
+# It is likely that src/Makevars.in should use $(MAIN_LINK) from lib/R/etc/Makeconf
+# for this instead of having to pass it in via the PKG_LIBS environment variable.
+if [[ ${HOST} =~ .*darwin.* ]]; then
   PKG_LIBS="-L${PREFIX}/lib" $R CMD INSTALL --build .
 else
   # .. on Linux the situation is even worse:
